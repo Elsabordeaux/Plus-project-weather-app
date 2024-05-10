4092361c3e47e341c2821cd2805befe7259443cd
Elsabordeaux <snowpaws85@gmail.com>
2024-05-10 22:16:19 +0100

    added Api search functions

--- src/script.js
+++ src/script.js
@@ -1,9 +1,28 @@
+function displayWeather(response) {
+  let displayedTemperature = document.querySelector(
+    "#displayed-temperature-value"
+  );
+  let currentTemperature = response.data.temperature.current;
+  displayedTemperature.innerHTML = Math.round(currentTemperature);
+  let cityDisplayed = document.querySelector("#displayed-city");
+  searchedCity = response.data.city;
+  cityDisplayed.innerHTML = searchedCity;
+}
+
+function searchCity(city) {
+  let apiKey = `dd83fab04t350832b43f1o8b985e5044`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityDisplayed = document.querySelector("#displayed-city");
-  cityDisplayed.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
