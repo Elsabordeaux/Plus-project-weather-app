5c78b06c43f671288cc2239268e33b19acb96cfc
Elsabordeaux <snowpaws85@gmail.com>
2024-05-12 20:38:11 +0100

    Added more API data information

--- src/script.js
+++ src/script.js
@@ -3,10 +3,50 @@ function displayWeather(response) {
     "#displayed-temperature-value"
   );
   let currentTemperature = response.data.temperature.current;
-  displayedTemperature.innerHTML = Math.round(currentTemperature);
   let cityDisplayed = document.querySelector("#displayed-city");
+  let conditionsDisplayed = document.querySelector("#conditions");
+  let humidityDisplayed = document.querySelector("#humidity");
+  let windDisplayed = document.querySelector("#wind-speed");
+  let timeDisplayed = document.querySelector("#day-and-time");
+
+  let iconLeft = document.querySelector("#icon-left");
+  let iconRight = document.querySelector("#icon-right");
+  let date = new Date(response.data.time * 1000);
   searchedCity = response.data.city;
+  localConditions = response.data.condition.description;
+  localHumidity = response.data.temperature.humidity;
+  localWindSpeed = response.data.wind.speed;
+  localIcon = response.data.condition.icon_url;
+
   cityDisplayed.innerHTML = searchedCity;
+  displayedTemperature.innerHTML = Math.round(currentTemperature);
+  conditionsDisplayed.innerHTML = localConditions;
+  timeDisplayed.innerHTML = formatDate(date);
+  humidityDisplayed.innerHTML = `${localHumidity}%`;
+  windDisplayed.innerHTML = `${localWindSpeed}km/h`;
+  iconRight.innerHTML = `<img src="${localIcon}" class="displayed-emoji-right">`;
+  iconLeft.innerHTML = `<img src="${localIcon}" class= "displayed-emoji-left">`;
+}
+
+function formatDate(date) {
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hour}:${minutes}`;
 }
 
 function searchCity(city) {
